455f593f909473dcbbe684e892b8b9d2444f85c5
Alberto Rico <alrico88@gmail.com>
2023-10-12 22:33:36 +0200

    fix: only use codemirror drop event

--- components/TextInput.vue
+++ components/TextInput.vue
@@ -2,15 +2,12 @@
 codemirror.form-control(
   v-model='enteredText',
   line-numbers,
-  @drop.prevent="handleDrop",
-  @dragover.prevent,
   :style="style",
   :extensions="extensions"
 )
 </template>
 
 <script setup lang="ts">
-import { readAsText } from "promise-file-reader";
 import { Codemirror } from "vue-codemirror";
 
 const props = defineProps<{
@@ -20,10 +17,4 @@ const props = defineProps<{
 const { style, extensions } = useCodeStyle();
 
 const enteredText = useVModel(props, "text");
-
-async function handleDrop(e: DragEvent): Promise<void> {
-  if (e.dataTransfer) {
-    enteredText.value = await readAsText(e.dataTransfer.files[0]);
-  }
-}
 </script>
